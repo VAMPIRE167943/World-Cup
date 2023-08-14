f64c919ba713cb8b8665fc6bb13d55d61025dbbb
Step-Creeper <jason.jordaan@sintrex.com>
2023-08-14 13:58:46 +0200

    work

--- routes/UsersRouter.js
+++ routes/UsersRouter.js
@@ -4,8 +4,14 @@ var router = express.Router();
 // Base route: /users
 
 // Get All users
-router.get('/', function (req, res) {
-
+router.get('/', async function (req, res) {
+    try {
+        await Person.find();
+        res.json({message: "Success"});
+      } catch (err) {
+        console.error(err);
+        res.json({ error: 'An error occurred while fetching users.' });
+      }
 })
 
 // Get User details
@@ -25,6 +31,6 @@ router.post('/register', function (req, res) {
 
 // Assign teams
 router.patch('/assignTeams', function (req, res) {
-   
+
 })
 module.exports = router;
