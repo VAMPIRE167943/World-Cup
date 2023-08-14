7ca2056eae4d136c7c8295c30b2eff36baa45259
Step-Creeper <jason.jordaan@sintrex.com>
2023-08-14 10:07:21 +0200

    Refine

--- models/person.js
+++ models/person.js
@@ -1,10 +1,11 @@
 var mongoose = require("mongoose");
+var {Schema} = mongoose
 
-var human = new mongoose.Schema({
-  name: String,
-  surname: String,
-  email: String,
-  password: String,
+var human = new Schema({
+  name: {type: String, required: true, lowercase: true},
+  surname: {type: String, required: true, lowercase: true},
+  email: {type: String, required: true, unique: true},
+  password: {type: String, required: true},
   teams: Array,
 });
 
