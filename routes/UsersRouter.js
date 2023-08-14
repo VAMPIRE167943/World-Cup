4eb3df86acf706113983ee153c15578980cabcdb
Step-Creeper <jason.jordaan@sintrex.com>
2023-08-14 14:01:37 +0200

    stop breaking my branches

--- routes/UsersRouter.js
+++ routes/UsersRouter.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 // Base route: /users
 
-// Get All users
+// Get all users
 router.get('/', async function (req, res) {
     try {
         await Person.find();
@@ -14,7 +14,7 @@ router.get('/', async function (req, res) {
       }
 })
 
-// Get User details
+// Get user details
 router.get('/:userEmail', function (req, res) {
 
 })
@@ -25,8 +25,21 @@ router.post('/checkCred', function (req, res) {
 })
 
 // Registers user
-router.post('/register', function (req, res) {
-
+router.post('/register', async function (req, res) {
+    try {
+        const { name, surname, email, password } = req.body;
+        const newPerson = new Person({
+          name: name,
+          surname: surname,
+          email: email,
+          password: password,
+        });
+        await newPerson.save();
+        res.json("You have given birth to a new person.");
+      } catch (err) {
+        console.error(err);
+        res.json("Just wasn't meant to be...");
+      }
 })
 
 // Assign teams
