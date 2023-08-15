fd6ff8628e6d16a8fd655a76b547aa7c99343a85
Step-Creeper <jason.jordaan@sintrex.com>
2023-08-15 08:22:05 +0200

    Teams CRUD operations

--- routes/TeamsRouter.js
+++ routes/TeamsRouter.js
@@ -1,30 +1,61 @@
 var express = require('express');
 var router = express.Router();
+var {connect} = require("../mongo.js")
+var Team = require("../models/teams.js")
 
 // Base route: /teams
 
 // Gets all teams
-router.get('/', function (req, res)
+router.get('/', async function (req, res, next)
 {
-
+    try{
+        await connect()
+        await Team.find()
+        res.status(200).json({message: "Gottemmmm"})
+    }catch(err){
+        console.log(err)
+        next(err)
+    }
 })
 
 // Get Shortlist
-router.get('/shortlist', function ()
+router.get('/shortlist', async function (req, res, next)
 {
-
+    try{
+        await connect()
+    }catch(err){
+        console.log(err)
+        next(err)
+    }
 })
 
 // Get a specified team info
-router.get('/:teamName', function (req, res)
+router.get('/:teamName', async function (req, res, next)
 {
-
+    try{
+        await connect()
+        var team_name = req.params.team_name
+        var team = await Team.findOne({team_name: team_name})
+        if(!team){
+            return res.status(404).json({error: "You sure they even exist? Cause not found..."})
+        }
+        res.status(200).json({data: team, message: "Gottem!"})
+    }catch(err){
+        console.log(err)
+        next(err)
+    }
 })
 
 // get matches by team
-router.get('/matches/:teamName', function (req, res)
+router.get('/matches/:teamName', async function (req, res, next)
 {
-
+    try{
+        await connect()
+        
+    }catch(err){
+        console.log(err)
+        next(err)
+    }
 })
 
 module.exports = router;
--- routes/UsersRouter.js
+++ routes/UsersRouter.js
@@ -6,59 +6,74 @@ var router = express.Router();
 // Base route: /users
 
 // Get all users
-router.get("/", async function (req, res, next) {
-  try {
+router.get("/", async function (req, res, next)
+{
+  try
+  {
     await connect();
     await Person.find();
     res.status(200).json({ message: "Gotteeeeem" });
-  } catch (err) {
+  } catch (err)
+  {
     console.error(err);
     next(err);
   }
 });
 
 // Get user details
-router.get("/:userEmail", async function (req, res, next) {
-  try {
+router.get("/:userEmail", async function (req, res, next)
+{
+  try
+  {
     await connect();
     var email = req.params.email;
     var person = await Person.findOne({ email: email });
-    if (!person) {
+    if (!person)
+    {
       return res
         .status(404)
         .json({ error: "Looks like this one was miscarried..." });
     }
     res.status(200).json({ data: person, message: "Gottem!" });
-  } catch (err) {
+  } catch (err)
+  {
     console.log(err);
     next(err);
   }
 });
 
 // Check credentials
-router.post("/checkCred", async function (req, res, next) {
-  try {
-    await connect();
+router.post("/checkCred", async function (req, res, next)
+{
+  try
+  {
+    await connect()
     var { email, password } = req.body;
     var person = await Person.findOne({ email: email });
-    if (!person) {
+    if (!person)
+    {
       return res
         .status(404)
         .json({ message: "Probably playing hide and seek again..." });
     }
-    if (person.password !== password) {
+    if (person.password !== password)
+    {
       return res.status(404).json({ message: "Stranger danger!" });
     }
     res.status(200).json({ message: "Ah yes, welcome back" });
-  } catch (err) {
+  } catch (err)
+  {
     console.log(err);
     next(err);
   }
 });
 
 // Registers user
-router.post("/register", async function (req, res, next) {
-  try {
+router.post("/register", async function (req, res, next)
+{
+  try
+  {
+    await connect()
     const { name, surname, email, password } = req.body;
     const newPerson = new Person({
       name: name,
@@ -68,25 +83,30 @@ router.post("/register", async function (req, res, next) {
     });
     await newPerson.save();
     res.status(201).json({ message: "You have given birth to a new person." });
-  } catch (err) {
+  } catch (err)
+  {
     console.error(err);
     next(err);
   }
 });
 
 // Assign teams
-router.patch("/assignTeams", async function (req, res, next) {
-  try {
+router.patch("/assignTeams", async function (req, res, next)
+{
+  try
+  {
     await connect();
     var { email, teams } = req.body;
     var person = await Person.findOne({ email: email });
-    if (!person) {
+    if (!person)
+    {
       return res
         .status(404)
         .json({ error: "Looks like this one was miscarried..." });
     }
     person.teams = teams;
-  } catch (err) {
+  } catch (err)
+  {
     console.log(err);
     next(err);
   }
