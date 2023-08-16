dc4c2de1dc91c2b5d25d6301f441cc9b0c33ca0a
Step-Creeper <jason.jordaan@sintrex.com>
2023-08-16 11:06:30 +0200

    Model update

--- models/teams.js
+++ models/teams.js
@@ -5,7 +5,8 @@ var team = new Schema({
   team_name: { type: String, lowercase: true },
   team_id: { type: Number, unique: true },
   country_name: { type: String, lowercase: true },
-  country_code: { type: String, uppercase: true }
+  country_code: { type: String, uppercase: true },
+  tier: {type: Number}
 });
 
 const Team = mongoose.model("Team", team);
