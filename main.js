91ca57a45826adfcf6fa272426a073c3c602b8e2
Step-Creeper <jason.jordaan@sintrex.com>
2023-08-23 10:07:03 +0200

    .

--- main.js
+++ main.js
@@ -40,15 +40,13 @@ function matches(){
         var birb = await connect()
         const count = await birb.collection("matches").countDocuments({})
         if(count > 0){
-           await birb.collection("matches").deleteMany({})
-           await birb.collection("matches").insertMany(res)
-           console.log("surprise motherfucker")
-         }else {
-            console.log("Why are we still here, just to suffer")
-            await birb.collection("matches").insertMany(res)
-         }
-
-        console.log("RESPECT++")
+          await birb.collection("matches").deleteMany({})
+          await birb.collection("matches").insertMany(res)
+          console.log("surprise motherfucker")
+        }else {
+          console.log("Why are we still here? Just to suffer? Every night, I can feel my leg... And my arm... even my fingers... The body I've lost... the comrades I've lost... won't stop hurting... It's like they're all still there. You feel it, too, don't you? I'm gonna make them give back our past!")
+          await birb.collection("matches").insertMany(res)
+        }
     }catch(err){
         console.log(err)
     }
